b83cd338d09b59e9b59baa072049b639772e7738
Irving Gonzalez <irving.agm.ico@gmail.com>
2020-05-06 13:36:26 -0500

    Commit esqueleto proyecto

---
+++ src/mx/com/gmsystems/source/ClassA.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.com.gmsystems.source;
+
+/**
+ *
+ * @author iagonzalezm
+ */
+public class ClassA {
+    public void metodo(){
+        System.out.println("Ejecutando metodo de clase A");
+    }
+}
---
+++ src/proyectojavagittest/ProyectoJavaGitTest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectojavagittest;
+
+/**
+ *
+ * @author iagonzalezm
+ */
+public class ProyectoJavaGitTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Este es un proyecto que se sincronizara con Git para teste de control de versiones");
+    }
+    
+}
